79a31f5a5ae8ada50b3d811443458b89c1ac4173
leejimin <leejimin@umich.edu>
2023-04-11 17:58:44 -0400

    many updates

--- firebase-config.js
+++ firebase-config.js
@@ -1,73 +1,90 @@
-// *** Copied from firebase initialization page
-// Import firebase
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBaKzvmF2qJ8J65hUmVUE5P8lWOZ3yel8M",
+//   authDomain: "cashulater-7f0d7.firebaseapp.com",
+//   projectId: "cashulater-7f0d7",
+//   storageBucket: "cashulater-7f0d7.appspot.com",
+//   messagingSenderId: "449041159177",
+//   appId: "1:449041159177:web:6afb9ddb1ffc67cf8a14d8"
+// };
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBaKzvmF2qJ8J65hUmVUE5P8lWOZ3yel8M",
-  authDomain: "cashulater-7f0d7.firebaseapp.com",
-  projectId: "cashulater-7f0d7",
-  storageBucket: "cashulater-7f0d7.appspot.com",
-  messagingSenderId: "449041159177",
-  appId: "1:449041159177:web:6afb9ddb1ffc67cf8a14d8"
-};
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// ***
 
-// function signupHandle() {
-//   // reference button element
-//   const signupButton = document.getElementById('signupButton');
-//   // make a container for all the input fields
-//   const inputFieldsContainer = document.getElementById('inputFieldsContainer');
- 
-//   signupButton.addEventListener('click', () => {
-// 	// add simple input field for email
-// 	const emailInput = document.createElement('input');
-// 	emailInput.type = 'email';
-// 	emailInput.id = 'emailInput';
-// 	emailInput.placeholder = 'Email';
-    
-// 	// password
-// 	const passwordInput = document.createElement('input');
-// 	passwordInput.type = 'password';
-// 	passwordInput.id = 'passwordInput';
-// 	passwordInput.placeholder = 'Password';
- 
-// 	// add simple submit button
-// 	const submitButton = document.createElement('button');
-// 	submitButton.id = 'submitButton';
-// 	submitButton.textContent = 'Submit';
- 
-// 	// Append input fields and submit button to the container
-// 	inputFieldsContainer.appendChild(emailInput);
-// 	inputFieldsContainer.appendChild(passwordInput);
-// 	inputFieldsContainer.appendChild(submitButton);
- 
-//   	// Add a click event listener to the submit button
-// 	submitButton.addEventListener('click', () => {
-//   	// Get the user's email and password from input fields
-//   	const email = emailInput.value;
-//   	const password = passwordInput.value;
- 
-//   	// Sign up with Firebase Authentication
-//   	firebase.auth().createUserWithEmailAndPassword(email, password)
-//     	.then((userCredential) => {
-//       	// Signed up successfully
-//       	const user = userCredential.user;
-//       	console.log('User signed up:', user);
-//     	})
-//     	.catch((error) => {
-//       	// Error occurred during sign up
-//       	console.error('Error signing up:', error);
-//     	});
-// 	});
-//   });
-// }
-
-// // Ensure the DOM has been loaded before attaching event listeners
-// document.addEventListener('DOMContentLoaded', () => {
-//   const signupButton = document.getElementById('signupButton');
-
-//   signupButton.addEventListener('click', signupHandle);
-// });
+function signup_pop(event) {
+    // prevent default <a> behavior
+    event.preventDefault();
+  
+    // Show the modal
+    const signupModal = document.getElementById('signupModal');
+    signupModal.classList.add('is-active');
+  
+    // Add the email and password input fields
+    const inputFieldsContainer = document.getElementById('inputFieldsContainer');
+  
+    // Clear the inputFieldsContainer before appending new input fields
+    inputFieldsContainer.innerHTML = '';
+  
+    const emailInput = document.createElement('input');
+    emailInput.type = 'email';
+    emailInput.id = 'emailInput';
+    emailInput.placeholder = 'Email';
+    emailInput.className = 'input';
+  
+    const passwordInput = document.createElement('input');
+    passwordInput.type = 'password';
+    passwordInput.id = 'passwordInput';
+    passwordInput.placeholder = 'Password';
+    passwordInput.className = 'input';
+  
+    inputFieldsContainer.appendChild(emailInput);
+    inputFieldsContainer.appendChild(passwordInput);
+  }
+  
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    const signupButton = document.getElementById('signupButton');
+    const closeModal = document.getElementById('closeModal');
+    const submitButton = document.getElementById('submitSignup');
+    const deleteButton = document.getElementById('cancelSignup');
+  
+    // Show the modal
+    signupButton.addEventListener('click', signup_pop);
+  
+    // Close the modal when the "Cancel" or "X" button is clicked
+    closeModal.addEventListener('click', closeSignupModal);
+    deleteButton.addEventListener('click', closeSignupModal);
+  
+    // Sign up when the "Submit" button is clicked
+    submitButton.addEventListener('click', submitSignupHandler);
+  });
+  
+  // Close the sign-up modal and clear input fields
+  function closeSignupModal() {
+    const signupModal = document.getElementById('signupModal');
+    signupModal.classList.remove('is-active');
+  }
+  
+  function submitSignupHandler() {
+    const email = document.getElementById('emailInput').value;
+    const password = document.getElementById('passwordInput').value;
+  
+    console.log(email)
+    console.log(password)
+  
+    // Sign up with Firebase Authentication
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Signed up successfully
+        const user = userCredential.user;
+        console.log('User signed up:', user);
+  
+        // Close the modal
+        closeSignupModal();
+      })
+      .catch((error) => {
+        // Error occurred during sign up
+        console.error('Error signing up:', error);
+      });
+  }
--- script.js
+++ script.js
@@ -1,38 +1,63 @@
-// recent transactions chart
+function someFunc() {
+  addData();
+  showNotification();
+}
+
 function addData() {
   var newTransaction = document.getElementById("transaction-type1").value;
   var newExpenseType = '';
   if (newTransaction == "Spending") {
     newExpenseType = document.getElementById("expense-type").value;
   }
+
   var newAmount = document.getElementById("amount").value;
   var newRepeat = document.getElementById("repeatCheckbox");
   var newNotes = document.getElementById("notes").value
- 
+
   if (!(newAmount > 0)) {
     alert("Please enter a valid transaction amount!");
     return;
   }
 
+  // if(newTransaction == "Choose...") {
+  // alert("Please choose a valid transaction type!");
+  // }
+
   var newRow = document.createElement("tr");
+  var newCell0 = document.createElement("td");
   var newCell1 = document.createElement("td");
   var newCell2 = document.createElement("td");
   var newCell3 = document.createElement("td");
   var newCell4 = document.createElement("td");
   var newCell5 = document.createElement("td");
- 
+
+  const today = new Date();
+  const dateString = today.toLocaleDateString();
+
+  const currentMonth = today.getMonth(); // Get the current month index
+  var x = document.getElementById("graph-types").options;
+  document.getElementById("graph-types").value = x[currentMonth].text;
+
+
+  newCell0.innerHTML = dateString;
+  console.log(dateString)
   newCell1.innerHTML = newTransaction;
-  newCell2.innerHTML = newExpenseType;
+  if (newTransaction == "Spending") {
+    newCell2.innerHTML = newExpenseType;
+  } else {
+    newCell2.innerHTML = "N/A"
+  }
   newCell3.innerHTML = newAmount;
-  
+
   if (newRepeat.checked) {
-	  newCell4.innerHTML = "Yes";
+    newCell4.innerHTML = document.getElementById("addSelect").value;
   }
   else {
-	  newCell4.innerHTML = "No";
+    newCell4.innerHTML = "No";
   }
   newCell5.innerHTML = newNotes;
- 
+
+  newRow.append(newCell0);
   newRow.append(newCell1);
   newRow.append(newCell2);
   newRow.append(newCell3);
@@ -42,16 +67,29 @@ function addData() {
   document.getElementById("rows").appendChild(newRow);
 
   // resetting the values back to default
-  document.getElementById("transaction-type1").value = "Income";
+  if (document.getElementById("transaction-type1").value == "Income") {
+    //show the thing below
+    var spendingLabel = document.getElementById("spendingLabel");
+    var newExpense = document.getElementById("expenseType");
+    spendingLabel.style.display = 'block';
+    newExpense.style.display = 'block';
+    newExpense.classList.add('is-arrowless');
+  }
+  document.getElementById("transaction-type1").value = "Spending";
   document.getElementById("expense-type").value = "Wants";
   document.getElementById("amount").value = '';
   document.getElementById("repeatCheckbox").checked = false;
   document.getElementById('repeated').style.display = 'none';
   document.getElementById("notes").value = '';
-  
-  setTimeout(() => {
-    confirm("You have successfully entered a transaction!");
-  }, 100);
+}
+
+function showNotification() {
+  var notificationEl = document.querySelector(".notification-message");
+
+  notificationEl.classList.add("show");
+  setTimeout(function() {
+    notificationEl.classList.remove("show");
+  }, 1000);
 }
 
 // filtering function
@@ -70,22 +108,22 @@ function filterFunc() {
       } else {
         tr[i].style.display = "none";
       }
-    }       
+    }
   }
 }
 
 function openTab(evt, tabName) {
   var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabcontent.length; i++) {
-   	 tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByTagName("li");
-    for (i = 0; i < tablinks.length; i++) {
-   	 tablinks[i].classList.remove("is-active");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.classList.add("is-active");
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByTagName("li");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("is-active");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.classList.add("is-active");
 }
 
 function toggleSelect() {
@@ -93,8 +131,27 @@ function toggleSelect() {
   const repeatedDiv = document.getElementById('repeated');
 
   if (repeatCheckbox.checked) {
-	repeatedDiv.style.display = 'block';
+    repeatedDiv.style.display = 'block';
+    repeatedDiv.classList.add('is-arrowless');
   } else {
-	repeatedDiv.style.display = 'none';
+    repeatedDiv.style.display = 'none';
+    repeatedDiv.classList.remove('is-arrowless');
+  }
+}
+
+function changeExpenseType() {
+  var newTransaction = document.getElementById("transaction-type1").value;
+  var spendingLabel = document.getElementById("spendingLabel");
+  var newExpense = document.getElementById("expenseType");
+
+  if (newTransaction == "Spending") {
+    spendingLabel.style.display = 'block';
+    newExpense.style.display = 'block';
+    newExpense.classList.add('is-arrowless');
+  }
+  else {
+    spendingLabel.style.display = 'none';
+    newExpense.style.display = 'none';
+    newExpense.classList.add('is-arrowless');
   }
 }
